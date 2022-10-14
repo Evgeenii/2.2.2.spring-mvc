4efd29512dc022d224b740a2e82bcaded5918f69
Eugene Kazmin <varenkine@gmail.com>
2022-10-14 11:29:12 +0300

    работа над ошибками

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,15 +1,14 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.ServiceCarImp;
 
 @Controller
-@Component
 public class CarController {
     private ServiceCarImp serviceCarImp;
 
@@ -19,7 +18,7 @@ public class CarController {
     }
 
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model, @RequestParam(defaultValue = "5") int count) {
+    public String printCars(Model model, @RequestParam(defaultValue = "5") int count) {
         model.addAttribute("cars", serviceCarImp.findCar(count));
         return "cars";
     }
